db6daef5b755f78a62497449177435e362c4e3ea
sobchenyuk <ablehands@ukr.net>
2018-03-24 21:17:17 +0200

    feature NEW-functionality

--- admin/config.php
+++ admin/config.php
@@ -8,18 +8,18 @@ define('HTTP_CATALOG', 'http://192.168.0.101/');
 //define('HTTPS_CATALOG', 'https://skinali-printcolor.com/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/admin/');
-define('DIR_SYSTEM', '/var/www/system/');
-define('DIR_IMAGE', '/var/www/image/');
-define('DIR_LANGUAGE', '/var/www/admin/language/');
-define('DIR_TEMPLATE', '/var/www/admin/view/template/');
-define('DIR_CONFIG', '/var/www/system/config/');
-define('DIR_CACHE', '/var/www/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/system/storage/download/');
-define('DIR_LOGS', '/var/www/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/system/storage/upload/');
-define('DIR_CATALOG', '/var/www/catalog/');
+define('DIR_APPLICATION', '/var/www/skinali/admin/');
+define('DIR_SYSTEM', '/var/www/skinali/system/');
+define('DIR_IMAGE', '/var/www/skinali/image/');
+define('DIR_LANGUAGE', '/var/www/skinali/admin/language/');
+define('DIR_TEMPLATE', '/var/www/skinali/admin/view/template/');
+define('DIR_CONFIG', '/var/www/skinali/system/config/');
+define('DIR_CACHE', '/var/www/skinali/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/skinali/system/storage/download/');
+define('DIR_LOGS', '/var/www/skinali/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/skinali/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/skinali/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/skinali/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
