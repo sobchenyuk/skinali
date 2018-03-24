e75c1ff26beaee252e7e03917f8467dd41a85525
sobchenyuk <ablehands@ukr.net>
2018-03-24 21:09:31 +0200

    feature NEW-functionality

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://skinali-printcolor.com/admin/');
-define('HTTP_CATALOG', 'https://skinali-printcolor.com/');
+define('HTTP_SERVER', 'http://192.168.0.101/admin/');
+define('HTTP_CATALOG', 'http://192.168.0.101/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://skinali-printcolor.com/admin/');
-define('HTTPS_CATALOG', 'https://skinali-printcolor.com/');
+//define('HTTPS_SERVER', 'https://skinali-printcolor.com/admin/');
+//define('HTTPS_CATALOG', 'https://skinali-printcolor.com/');
 
 // DIR
-define('DIR_APPLICATION', '/home/printcol/skinali-printcolor.com/www/admin/');
-define('DIR_SYSTEM', '/home/printcol/skinali-printcolor.com/www/system/');
-define('DIR_IMAGE', '/home/printcol/skinali-printcolor.com/www/image/');
-define('DIR_LANGUAGE', '/home/printcol/skinali-printcolor.com/www/admin/language/');
-define('DIR_TEMPLATE', '/home/printcol/skinali-printcolor.com/www/admin/view/template/');
-define('DIR_CONFIG', '/home/printcol/skinali-printcolor.com/www/system/config/');
-define('DIR_CACHE', '/home/printcol/skinali-printcolor.com/www/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home/printcol/skinali-printcolor.com/www/system/storage/download/');
-define('DIR_LOGS', '/home/printcol/skinali-printcolor.com/www/system/storage/logs/');
-define('DIR_MODIFICATION', '/home/printcol/skinali-printcolor.com/www/system/storage/modification/');
-define('DIR_UPLOAD', '/home/printcol/skinali-printcolor.com/system/www/storage/upload/');
-define('DIR_CATALOG', '/home/printcol/skinali-printcolor.com/www/catalog/');
+define('DIR_APPLICATION', '/var/www/admin/');
+define('DIR_SYSTEM', '/var/www/system/');
+define('DIR_IMAGE', '/var/www/image/');
+define('DIR_LANGUAGE', '/var/www/admin/language/');
+define('DIR_TEMPLATE', '/var/www/admin/view/template/');
+define('DIR_CONFIG', '/var/www/system/config/');
+define('DIR_CACHE', '/var/www/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/system/storage/download/');
+define('DIR_LOGS', '/var/www/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
