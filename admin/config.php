3ec8f29af8086757d2e12144c474961ee6ba98f4
sobchenyuk <ablehands@ukr.net>
2018-03-25 00:02:46 +0200

    feature NEW-functionality

--- admin/config.php
+++ admin/config.php
@@ -1,11 +1,11 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://192.168.0.101/admin/');
-define('HTTP_CATALOG', 'https://192.168.0.101/');
+define('HTTP_SERVER', 'http://192.168.0.101/admin/');
+define('HTTP_CATALOG', 'http://192.168.0.101/');
 
 // HTTPS
-//define('HTTPS_SERVER', 'https://192.168.0.101/admin/');
-//define('HTTPS_CATALOG', 'https://192.168.0.101/');
+define('HTTPS_SERVER', 'https://192.168.0.101/admin/');
+define('HTTPS_CATALOG', 'https://192.168.0.101/');
 
 // DIR
 define('DIR_APPLICATION', '/var/www/skinali/admin/');
