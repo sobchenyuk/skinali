44bb0f93671fe2f8b8c78b2ffcc9413dae1008f5
sobchenyuk <ablehands@ukr.net>
2018-03-30 05:36:51 +0300

    feature MENU

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -241,7 +241,6 @@ class ControllerCommonHeader extends Controller {
 
         $data['content_top'] = $this->load->controller('common/content_top');
 
-        var_dump($data['content_top']);
 
 		// For page specific css
 		if (isset($this->request->get['route'])) {
