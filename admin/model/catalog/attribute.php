fdf8ed0f23c63308b5d1f6319b9db88113bdb1bb
sobchenyuk <ablehands@ukr.net>
2018-04-03 00:51:33 +0300

    feature ATTRIBUTE

--- admin/model/catalog/attribute.php
+++ admin/model/catalog/attribute.php
@@ -33,6 +33,12 @@ class ModelCatalogAttribute extends Model {
 		return $query->row;
 	}
 
+    public function getAttr(){
+        $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "attribute` WHERE 1");
+
+        return $query->row;
+    }
+
 	public function getAttributes($data = array()) {
 		$sql = "SELECT *, (SELECT agd.name FROM " . DB_PREFIX . "attribute_group_description agd WHERE agd.attribute_group_id = a.attribute_group_id AND agd.language_id = '" . (int)$this->config->get('config_language_id') . "') AS attribute_group FROM " . DB_PREFIX . "attribute a LEFT JOIN " . DB_PREFIX . "attribute_description ad ON (a.attribute_id = ad.attribute_id) WHERE ad.language_id = '" . (int)$this->config->get('config_language_id') . "'";
 
--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -246,7 +246,7 @@ class ControllerProductProduct extends Controller {
 
 
 
-        $attribute_id = $this->model_catalog_category->getAttr();
+        $attribute_id = $this->model_catalog_product->getAttr();
 
 //        foreach ($arrAttr as $item) {
 //
