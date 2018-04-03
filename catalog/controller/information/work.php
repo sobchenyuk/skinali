c3697eba5193999ec7d081b6cb6c87b8e89669fb
sobchenyuk <ablehands@ukr.net>
2018-04-04 01:59:23 +0300

    feature ATTRIBUTE

--- catalog/controller/information/work.php
+++ catalog/controller/information/work.php
@@ -3,6 +3,13 @@ class ControllerInformationWork extends Controller {
 
     public function index() {
 
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/information/work.tpl')) {
+            $this->template = $this->config->get('config_template') . '/template/information/work.tpl';
+            $this->data['template'] = $this->config->get('config_template');
+        } else {
+            $this->template = 'default/template/latest/latest.tpl';
+        }
+
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['column_right'] = $this->load->controller('common/column_right');
         $data['content_top'] = $this->load->controller('common/content_top');
