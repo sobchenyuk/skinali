15d74660caeeef956019c9db2b62fa0b0adcb44b
sobchenyuk <ablehands@ukr.net>
2018-04-04 02:05:36 +0300

    feature ATTRIBUTE

--- catalog/controller/information/work.php
+++ catalog/controller/information/work.php
@@ -3,6 +3,8 @@ class ControllerInformationWork extends Controller {
 
     public function index() {
 
+        echo 'work';
+
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['column_right'] = $this->load->controller('common/column_right');
         $data['content_top'] = $this->load->controller('common/content_top');
