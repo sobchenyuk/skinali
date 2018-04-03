a51ac95f626485727e776f5861d2f9a869af400f
sobchenyuk <ablehands@ukr.net>
2018-04-04 01:47:58 +0300

    feature ATTRIBUTE

---
+++ catalog/controller/information/work.php
@@ -0,0 +1,17 @@
+<?php
+class ControllerInformationWork extends Controller {
+
+    public function index() {
+        $data['continue'] = $this->url->link('common/home');
+
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['column_right'] = $this->load->controller('common/column_right');
+        $data['content_top'] = $this->load->controller('common/content_top');
+        $data['content_bottom'] = $this->load->controller('common/content_bottom');
+        $data['footer'] = $this->load->controller('common/footer');
+        $data['header'] = $this->load->controller('common/header');
+
+        $this->response->setOutput($this->load->view('information/work', $data));
+    }
+
+}
