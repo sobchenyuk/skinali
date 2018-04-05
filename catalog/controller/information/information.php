869b64b750f4c1b9d55432f96dc479dfe44f34b1
sobchenyuk <ablehands@ukr.net>
2018-04-05 03:47:28 +0300

    finish ourwork

--- catalog/controller/information/information.php
+++ catalog/controller/information/information.php
@@ -439,7 +439,8 @@ class ControllerInformationInformation extends Controller {
 
 			echo $_SERVER['REQUEST_URI'];
 
-			var_dump($_GET);
+			var_dump($_GET["_route_"]);
+            $_GET["_route_"];
 
             $data['smenu_galleryrb'] = $this->getMenu(4);
 
