ebedfc0d6fd9cc008340d483e0e163dea9a192ea
sobchenyuk <ablehands@ukr.net>
2018-04-10 15:37:56 +0300

    finish adaptation

--- catalog/view/theme/printcolor/template/common/ratings/rating.js
+++ catalog/view/theme/printcolor/template/common/ratings/rating.js
@@ -16,6 +16,9 @@
         console.log(total);
         console.log(rating);
 
+        total++;
+        var abs = ((rating + num) / total);
+        abs = (abs ^ 0) === abs ? abs : abs.toFixed(1);
         $el.parent().parent().find('span.rating-text').html('(' + total + ' ' + decOfNum(total, ['голос', 'голоса', 'голосов']) + ', в среднем: ' + abs + ' из 5)');
 
 
