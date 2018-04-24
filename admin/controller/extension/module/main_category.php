372c38f334db7bd4eb817f5577f98ca80bebf5b6
sobchenyuk <ablehands@ukr.net>
2018-04-24 10:03:18 +0300

    RELEASE

--- admin/controller/extension/module/main_category.php
+++ admin/controller/extension/module/main_category.php
@@ -3,128 +3,37 @@
 class ControllerExtensionModuleMainCategory extends Controller {
 
     private $error = array();
-    private $category_id = 0;
-    private $path = array();
 
-    public function index(){
+    public function index() {
+        $this->load->language('extension/module/category');
 
-        $this->load->language('catalog/category');
+        $this->document->setTitle($this->language->get('heading_title'));
 
-        $this->document->setTitle('Основные категории');
+        $this->load->model('setting/setting');
 
-        $this->load->model('catalog/category');
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+            $this->model_setting_setting->editSetting('category', $this->request->post);
 
-        $this->getList();
-    }
+            $this->session->data['success'] = $this->language->get('text_success');
 
-    private function getCategories($parent_id, $parent_path = '', $indent = '') {
-        $category_id = array_shift($this->path);
-
-        $output = array();
-
-        static $href_category = null;
-        static $href_action = null;
-
-        if ($href_category === null) {
-            $href_category = $this->url->link('catalog/main_category', 'token=' . $this->session->data['token'] . '&path=', 'SSL');
-            $href_action = $this->url->link('catalog/main_category/update', 'token=' . $this->session->data['token'] . '&category_id=', 'SSL');
+            $this->response->redirect($this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true));
         }
 
-        $url = '';
+        $data['heading_title'] = $this->language->get('heading_title');
 
-        if (isset($this->request->get['sort'])) {
-            $url .= '&sort=' . $this->request->get['sort'];
-        }
+        $data['text_edit'] = $this->language->get('text_edit');
+        $data['text_enabled'] = $this->language->get('text_enabled');
+        $data['text_disabled'] = $this->language->get('text_disabled');
 
-        if (isset($this->request->get['order'])) {
-            $url .= '&order=' . $this->request->get['order'];
-        }
+        $data['entry_status'] = $this->language->get('entry_status');
 
-        if (isset($this->request->get['page'])) {
-            $url .= '&page=' . $this->request->get['page'];
-        }
+        $data['button_save'] = $this->language->get('button_save');
+        $data['button_cancel'] = $this->language->get('button_cancel');
 
-        $results = $this->model_catalog_category->getCategoriesByParentId($parent_id);
-
-        foreach ($results as $result) {
-            $path = $parent_path . $result['category_id'];
-
-            $href = ($result['children']) ? $href_category . $path : '';
-
-            $name = $result['name'];
-
-            if ($category_id == $result['category_id']) {
-                $name = '<b>' . $name . '</b>';
-
-                $data['breadcrumbs'][] = array(
-                    'text'      => $result['name'],
-                    'href'      => $href,
-                    'separator' => ' :: '
-                );
-
-                $href = '';
-            }
-
-            $selected = isset($this->request->post['selected']) && in_array($result['category_id'], $this->request->post['selected']);
-
-            $action = array();
-
-            $action[] = array(
-                'text' => $this->language->get('text_edit'),
-                'href' => $href_action . $result['category_id']
-            );
-
-            $output[$result['category_id']] = array(
-                'category_id' => $result['category_id'],
-                'name'        => $name,
-                'sort_order'  => $result['sort_order'],
-                'selected'    => $selected,
-                'action'      => $action,
-                'edit'        => $this->url->link('catalog/main_category/edit', 'token=' . $this->session->data['token'] . '&category_id=' . $result['category_id'] . $url, 'SSL'),
-                'delete'      => $this->url->link('catalog/main_category/delete', 'token=' . $this->session->data['token'] . '&category_id=' . $result['category_id'] . $url, 'SSL'),
-                'href'        => $href,
-                'indent'      => $indent
-            );
-
-            if ($category_id == $result['category_id']) {
-                $output += $this->getCategories($result['category_id'], $path . '_', $indent . str_repeat('&nbsp;', 8));
-            }
-        }
-
-        return $output;
-    }
-
-    protected function getList() {
-        if (isset($this->request->get['sort'])) {
-            $sort = $this->request->get['sort'];
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
         } else {
-            $sort = 'name';
-        }
-
-        if (isset($this->request->get['order'])) {
-            $order = $this->request->get['order'];
-        } else {
-            $order = 'ASC';
-        }
-
-        if (isset($this->request->get['page'])) {
-            $page = $this->request->get['page'];
-        } else {
-            $page = 1;
-        }
-
-        $url = '';
-
-        if (isset($this->request->get['sort'])) {
-            $url .= '&sort=' . $this->request->get['sort'];
-        }
-
-        if (isset($this->request->get['order'])) {
-            $url .= '&order=' . $this->request->get['order'];
-        }
-
-        if (isset($this->request->get['page'])) {
-            $url .= '&page=' . $this->request->get['page'];
+            $data['error_warning'] = '';
         }
 
         $data['breadcrumbs'] = array();
@@ -135,115 +44,38 @@ class ControllerExtensionModuleMainCategory extends Controller {
         );
 
         $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('Основные категории'),
-            'href' => $this->url->link('catalog/main_category', 'token=' . $this->session->data['token'] . $url, true)
+            'text' => $this->language->get('text_extension'),
+            'href' => $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true)
         );
 
-        $data['add'] = $this->url->link('catalog/main_category/add', 'token=' . $this->session->data['token'] . $url, true);
-        $data['delete'] = $this->url->link('catalog/main_category/delete', 'token=' . $this->session->data['token'] . $url, true);
-        $data['repair'] = $this->url->link('catalog/main_category/repair', 'token=' . $this->session->data['token'] . $url, true);
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('heading_title'),
+            'href' => $this->url->link('extension/module/category', 'token=' . $this->session->data['token'], true)
+        );
 
-        $data['categories'] = array();
+        $data['action'] = $this->url->link('extension/module/category', 'token=' . $this->session->data['token'], true);
 
-        if (isset($this->request->get['path'])) {
-            if ($this->request->get['path'] != '') {
-                $this->path = explode('_', $this->request->get['path']);
-                $this->category_id = end($this->path);
-                $this->session->data['path'] = $this->request->get['path'];
-            } else {
-                unset($this->session->data['path']);
-            }
-        } elseif (isset($this->session->data['path'])) {
-            $this->path = explode('_', $this->session->data['path']);
-            $this->category_id = end($this->path);
-        }
+        $data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true);
 
-        $data['categories'] = $this->getCategories(0);
-
-        $category_total = count($data['categories']);
-
-        $data['heading_title'] = $this->language->get('heading_title');
-
-        $data['text_list'] = $this->language->get('text_list');
-        $data['text_no_results'] = $this->language->get('text_no_results');
-        $data['text_confirm'] = $this->language->get('text_confirm');
-
-        $data['column_name'] = $this->language->get('column_name');
-        $data['column_sort_order'] = $this->language->get('column_sort_order');
-        $data['column_action'] = $this->language->get('column_action');
-
-        $data['button_add'] = $this->language->get('button_add');
-        $data['button_edit'] = $this->language->get('button_edit');
-        $data['button_delete'] = $this->language->get('button_delete');
-        $data['button_rebuild'] = $this->language->get('button_rebuild');
-
-        if (isset($this->error['warning'])) {
-            $data['error_warning'] = $this->error['warning'];
+        if (isset($this->request->post['category_status'])) {
+            $data['category_status'] = $this->request->post['category_status'];
         } else {
-            $data['error_warning'] = '';
+            $data['category_status'] = $this->config->get('category_status');
         }
-
-        if (isset($this->session->data['success'])) {
-            $data['success'] = $this->session->data['success'];
-
-            unset($this->session->data['success']);
-        } else {
-            $data['success'] = '';
-        }
-
-        if (isset($this->request->post['selected'])) {
-            $data['selected'] = (array)$this->request->post['selected'];
-        } else {
-            $data['selected'] = array();
-        }
-
-        $url = '';
-
-        if ($order == 'ASC') {
-            $url .= '&order=DESC';
-        } else {
-            $url .= '&order=ASC';
-        }
-
-        if (isset($this->request->get['page'])) {
-            $url .= '&page=' . $this->request->get['page'];
-        }
-
-        $data['sort_name'] = $this->url->link('catalog/cmain_category', 'token=' . $this->session->data['token'] . '&sort=name' . $url, true);
-        $data['sort_sort_order'] = $this->url->link('catalog/main_category', 'token=' . $this->session->data['token'] . '&sort=sort_order' . $url, true);
-
-        $url = '';
-
-        if (isset($this->request->get['sort'])) {
-            $url .= '&sort=' . $this->request->get['sort'];
-        }
-
-        if (isset($this->request->get['order'])) {
-            $url .= '&order=' . $this->request->get['order'];
-        }
-
-        if (isset($this->request->get['path'])) {
-            $url .= '&path=' . $this->request->get['path'];
-        }
-
-        $pagination = new Pagination();
-        $pagination->total = $category_total;
-        $pagination->page = $page;
-        $pagination->limit = $this->config->get('config_limit_admin');
-        $pagination->url = $this->url->link('catalog/main_category', 'token=' . $this->session->data['token'] . $url . '&page={page}', true);
-
-        $data['pagination'] = $pagination->render();
-
-        $data['results'] = sprintf($this->language->get('text_pagination'), ($category_total) ? (($page - 1) * $this->config->get('config_limit_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_limit_admin')) > ($category_total - $this->config->get('config_limit_admin'))) ? $category_total : ((($page - 1) * $this->config->get('config_limit_admin')) + $this->config->get('config_limit_admin')), $category_total, ceil($category_total / $this->config->get('config_limit_admin')));
-
-        $data['sort'] = $sort;
-        $data['order'] = $order;
 
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['footer'] = $this->load->controller('common/footer');
 
-        $this->response->setOutput($this->load->view('catalog/category_list', $data));
+        $this->response->setOutput($this->load->view('extension/module/category', $data));
+    }
+
+    protected function validate() {
+        if (!$this->user->hasPermission('modify', 'extension/module/category')) {
+            $this->error['warning'] = $this->language->get('error_permission');
+        }
+
+        return !$this->error;
     }
 
 }
